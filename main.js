537df3a7b02e685191ef976c69f4e45ece92b00b
Napoleon-Jahmiu <Napoleonjay09mail.com>
2020-03-22 18:26:13 +0100

    working on screen size lower than 321px and also getting started with modal

---
+++ main.js
@@ -0,0 +1,19 @@
+// Get Modal Element
+var modal = document.getElementById('simpleModal');
+
+// Get Open Modal button
+var modalBtn = document.getElementsByClassName('modalBtn');
+
+// Get CLose Modal button
+var CloseBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for a click event (open Modal)
+modalBtn.addEventListener('click', openModal);
+
+// Open Modal Section (The form for the event)
+modal.style.display = 'block';
+
+// Close Modal section (Form for the events)
+
+
+
